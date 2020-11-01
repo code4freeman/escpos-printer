a3f33c7d2a7d4849131eb029501588b22dd469d5
lilindog <lilin@lilin.site>
2020-11-01 22:46:25 +0800

    继续研究ESC*打印位图, 看源码有点眉目了

--- test.js
+++ test.js
@@ -53,15 +53,19 @@ Image.prototype.toBitmap = function(density) {
     var x, y, b, l, i;
     var c = density / 8;
   
-    // n blocks of lines
+    // 图片高度除以密度的出行
     var n = Math.ceil(this.size.height / density);
-  
+
+    //循环每行
     for (y = 0; y < n; y++) {
-      // line data
+
+      // 每行的数据集合
       ld = result[y] = [];
-  
+      
+      //循环每行的列
       for (x = 0; x < this.size.width; x++) {
-  
+        
+        //使用密度在列循环中取数据
         for (b = 0; b < density; b++) {
           i = x * c + (b >> 3);
   
@@ -79,3 +83,83 @@ Image.prototype.toBitmap = function(density) {
       }
     }
   
+    // 3
+    printImageBuffer(width, height, data) {
+      this.buffer = null;
+  
+      // Get pixel rgba in 2D array
+      var pixels = [];
+      for (var i = 0; i < height; i++) {
+        var line = [];
+        for (var j = 0; j < width; j++) {
+          var idx = (width * i + j) << 2;
+          line.push({
+            r: data[idx],
+            g: data[idx + 1],
+            b: data[idx + 2],
+            a: data[idx + 3]
+          });
+        }
+        pixels.push(line);
+      }
+  
+  
+      var imageBuffer_array = [];
+      for (var i = 0; i < height; i++) {
+        for (var j = 0; j < Math.ceil(width / 8); j++) {
+          var byte = 0x0;
+          for (var k = 0; k < 8; k++) {
+            var pixel = pixels[i][j * 8 + k];
+  
+            // Image overflow
+            if (pixel === undefined) {
+              pixel = {
+                a: 0,
+                r: 0,
+                g: 0,
+                b: 0
+              };
+            }
+  
+            if (pixel.a > 126) { // checking transparency
+              var grayscale = parseInt(0.2126 * pixel.r + 0.7152 * pixel.g + 0.0722 * pixel.b);
+  
+              if (grayscale < 128) { // checking color
+                var mask = 1 << 7 - k; // setting bitwise mask
+                byte |= mask; // setting the correct bit to 1
+              }
+            }
+          }
+  
+          imageBuffer_array.push(byte);
+          // imageBuffer = Buffer.concat([imageBuffer, Buffer.from([byte])]);
+        }
+      }
+  
+      let imageBuffer = Buffer.from(imageBuffer_array);
+  
+      // Print raster bit image
+      // GS v 0
+      // 1D 76 30	m	xL xH	yL yH d1...dk
+      // xL = (this.width >> 3) & 0xff;
+      // xH = 0x00;
+      // yL = this.height & 0xff;
+      // yH = (this.height >> 8) & 0xff;
+      // https://reference.epson-biz.com/modules/ref_escpos/index.php?content_id=94
+  
+      // Check if width/8 is decimal
+      if (width % 8 != 0) {
+        width += 8;
+      }
+  
+      this.append(Buffer.from([0x1d, 0x76, 0x30, 48]));
+      this.append(Buffer.from([(width >> 3) & 0xff]));
+      this.append(Buffer.from([0x00]));
+      this.append(Buffer.from([height & 0xff]));
+      this.append(Buffer.from([(height >> 8) & 0xff]));
+  
+      // append data
+      this.append(imageBuffer);
+  
+      return this.buffer;
+    }
